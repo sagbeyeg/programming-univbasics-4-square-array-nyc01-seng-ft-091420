acde5ea235f8ab303f0a130fd11f2ba34f6db135
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-26 01:41:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   count = 0 
   
   while count < array.length do
-    
-    
-    Math.sqrt(number)
+    array[count] = Math.sqrt(array[count])
     counter += 1 
   end
 end
